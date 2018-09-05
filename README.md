e6449889dd8b8e047bb5292ee25330632f274edd
Mjohnwright <mjohnwright@hotmail.com>
2018-09-05 13:27:07 -0400

    fina;

---
+++ README.md
@@ -0,0 +1,29 @@
+CLOCK TOWER BELL COUNTER
+
+User Story
+
+As a clock enthusiast, I want to know how many times a clock tower will ring its bell between two given times, so that I can plan to sing along.
+
+Background
+A clock tower will ring its bell every hour, on the hour, a number of times equal to the number indicated by the hours hand.
+
+Example
+At 3pm, the clock tower will ring the bell three times. At midnight, it will ring the bell 12 times.
+
+Technical Specification
+** I should be able to input a start time and end time into the browser. Both times will be in
+   twenty-four hour notation, such as 12:00 for noon and 15:30 for half-past three in the afternoon.
+** If a user enters a number that is not in the expected format, an error message should be
+   displayed.
+** The UI should display the total number of times the clock tower will ring its bell between the
+   two provided times.
+
+Notes
+1. If either time is on the hour (ex. 14:00) then you should count the bell rings that would occur at that hour.
+2. If both times are equal, treat it as if twenty-four hours will pass and not that the two times are the same time on the same day.
+
+Test Cases
+1. **INPUT** $startTime = '2:00'; $endTime = '3:00'; **OUTPUT** 5
+2. **INPUT** $startTime = '14:00'; $endTime = '15:00'; **OUTPUT** 5
+3. **INPUT** $startTime = '14:23'; $endTime = '15:42'; **OUTPUT** 3
+4. **INPUT** $startTime = '23:00'; $endTime = '1:00'; **OUTPUT** 24
--- assets/logic.js
+++ assets/logic.js
@@ -2,96 +2,141 @@
 // validate for correct format
 // convert those values to integers
 // iterate through the difference and accumulate the total
-// return the total number
+// return the total number to DOM
 
 // alert("connected");
 
-var Input;
-var endTimeInput;
 var clockStrikes = 0;
 
 /*****************************
-         CLICK EVENTS
+         CLICK EVENT
 *****************************/
-// // SUBMIT BUTTON TO CAPTURE TIME
+// // SUBMIT BUTTON TO CAPTURE TIME variables
 $("#button").on("click", function() {
   event.preventDefault();
   // Grabs user input
   startTimeInput = $("#startTime")
     .val()
     .trim();
-  console.log(Input);
+  // console.log(startTimeInput);
 
   endTimeInput = $("#endTime")
     .val()
     .trim();
-  console.log(endTimeInput);
+  //console.log(endTimeInput);
 
   convert(startTimeInput, endTimeInput);
 });
 
-// Converts time to integers
+// Converts time to integers and processes clockStrike scenarios
 function convert(a, b) {
+  // grabs the first part of the START time input
   a = startTimeInput.split(":"); // 1
   startTime = parseInt(a[0]);
-  console.log("diff1 " + startTime);
+  //console.log("startTime = " + startTime);
 
+  // grabs the second part of the START time input
+  exception = parseInt(a[1]);
+  //console.log("exception = " + exception);
+
+  // grabs the first part of the END time input
   b = endTimeInput.split(":"); // 23
   endTime = parseInt(b[0]);
-  console.log("diff2 " + endTime);
+  //console.log("endTime " + endTime);
 
+  // calulation used to determine differnce in time inputs
   var timeDifference = Math.abs(startTime - endTime) + 1;
-  //   console.log("TOTAL after if statements = " + timeDifference);
+  console.log("TOTAL after if statements = " + timeDifference);
 
-  // START TIME = END TIME......24 HOURS
+  // *******************************
+  //        TIME SECENARIOS
+  // *******************************
+
+  // Scenario 0 - START TIME = END TIME......24 HOURS - clockStrikes = 78
+  console.log("scenario 0");
+
   if (startTime === endTime) {
+    $("#time").append("CLOCK STRIKES = 78");
     console.log("24 hours = 78");
+    return;
   }
 
-  // IF START TIME IS GREATER THAN END TIME
-  if (startTime > endTime ) {
-    console.log("(1 startTime > 12 && endTimeInput > 12)");
+  // Scenario 1 - startTime < 12 && endTime < 12 && startTime < endTime
+  if (startTime < 12 && endTime < 12 && startTime < endTime) {
+    console.log("scenario 1");
+    // 1:00 - 3:00
 
-    for (var i = 24; i >= startTime; i--) {
-        clockStrikes += i;
-        console.log("Higher than 24 = " + clockStrikes);
+    for (var i = 0; i <= endTime; i++) {
+      clockStrikes += i;
+
+      console.log("SECONED EX = " + clockStrikes);
     }
-    for (var j = 1; j <= endTime; j++) {
-        clockStrikes += j;
-        console.log("Higher than 12 = " + clockStrikes);
-        }
+    $("#time").append("CLOCK STRIKES = " + clockStrikes);
+    //startTimeInput = 0;
+    //endTimeInput = 0;
+    return;
   }
 
-  //   if (startTime > 12 || endTime > 12 && startTime > endTime) {
-  //     console.log("(startTime || endTime > 12 && startTime > endTime)");
+  // Scenario 2 - startTime > 12 && startTime > 12 && startTime < endTime
+  if (startTime > 12 && startTime > 12 && startTime < endTime) {
+    console.log("scenario 2");
+    // ex. 19:00 - 21:00 = 24
 
-  //     while (startTime >= endTime) {
-  //       for (var i = 0; i < timeDifference; i++) {
-  //         clockStrikes += startTime;
-  //         startTime--;
-  //         console.log("FIRST EX = " + clockStrikes);
-  //       }
-  //     }
-  //   }
+    for (var i = startTime; i <= endTime; i++) {
+      clockStrikes += startTime - 12;
+      startTime++;
+      console.log("ClockStrikes = " + clockStrikes);
+    }
+    $("#time").append("CLOCK STRIKES = " + clockStrikes);
+    return;
+  }
 
+  // Scenario 3 - startTime > 12 && endTime < 12
+  if (startTime > 12 && endTime < 12) {
+    console.log("scenario 3");
+    // ex. 22:00 - 2:00 = 36
 
-
-  if (endTime < 24 && endTime > startTime) {
-    console.log("(2 endTime > startTime)");
-    while (endTime >= startTime) {
-      for (var i = 0; i < timeDifference; i++) {
-        clockStrikes += endTime;
-        endTime--;
-        console.log("SECONED EX = " + clockStrikes);
+    for (var i = startTime; i <= endTime + 24; i++) {
+      if (i <= 24) {
+        clockStrikes += i - 12;
+        console.log("less than 12 = " + clockStrikes);
+      } else {
+        clockStrikes += i - 24;
+        console.log("more than 13 = " + clockStrikes);
       }
     }
+    $("#time").append("CLOCK STRIKES = " + clockStrikes);
+    return;
   }
 
-//   if (startTime > endTime && startTime + endTime > 24) {
-//     console.log("(3 startTime > endTime && startTime + endTime > 24)");
-//     for (var i = 24; i >= startTime; i--) {
-//       clockStrikes += i;
-//       console.log("Higher than 24 = " + clockStrikes);
-//     }
-//   }
+  // Scenario 4 - endTime >= 12 && startTime <= 12
+  if (endTime >= 12 && startTime <= 12) {
+    console.log("scenario 4");
+    // ex. 7:00 - 13:00 = 58
+
+    for (var i = endTime; i >= startTime; i--) {
+      if (i >= 13) {
+        clockStrikes += endTime - 12;
+        console.log("more than 12 = " + clockStrikes);
+        endTime--;
+      } else {
+        clockStrikes += endTime;
+        endTime--;
+        console.log(" less than 13 = " + clockStrikes);
+      }
+    }
+    $("#time").append("CLOCK STRIKES = " + clockStrikes);
+    return;
+  }
+
+  // Scenario 5 - endTime > 12 && startTime > 12
+  if (endTime > 12 && startTime > 12) {
+    console.log("scenario 5");
+    // 13:00 - 15:00 = 6
+
+    for (var i = startTime; i <= endTime; i++) clockStrikes += endTime - 12;
+    console.log("more than 12 = " + clockStrikes);
+  }
+  $("#time").append("CLOCK STRIKES = " + clockStrikes);
+  return;
 }
